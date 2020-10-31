333bf0e18bddf229e2c7183dc6d210e350f11f57
Francis <inmob.comercial@gmail.com>
2020-10-30 23:18:48 -0300

    Cod Input Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from './pages/SignIn';
 import GlobalStayle from './styles/globals';
 
 const App: React.FC = () => (
   <>
-  <h1>Hello World</h1>
+  <SignIn />
   <GlobalStayle/>
   </>
 );
