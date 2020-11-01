d095ac9f1989d4711916ecea11af51fe02143483
Francis <inmob.comercial@gmail.com>
2020-10-31 22:20:06 -0300

    Utilizando Urform

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+//import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStayle from './styles/globals';
 
 const App: React.FC = () => (
   <>
-  <SignIn />
+  <SignUp />
   <GlobalStayle/>
   </>
 );
