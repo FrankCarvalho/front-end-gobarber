43498d5cf0808284f88d03061823593759e93b21
Francis Carvalho <inmob.comercial@gmail.com>
2021-03-20 21:52:39 -0300

    Calendário e estilizações

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import React, { useCallback, useState } from 'react';
+import DayPicker, {DayModifiers} from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if(modifiers.available){
+      setSelectedDate(day);
+    }
+  }, []);
+
   const { signOut, user } = useAuth();
 
   return (
@@ -29,6 +51,104 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendadoos</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/55095394?s=460&u=81de13f8b108c2e90244398e64a77fee679fe3ef&v=4" alt="Francis Carvalho" />
+
+              <strong>Francis Carvalho</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/55095394?s=460&u=81de13f8b108c2e90244398e64a77fee679fe3ef&v=4" alt="Francis Carvalho" />
+
+                <strong>Francis Carvalho</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/55095394?s=460&u=81de13f8b108c2e90244398e64a77fee679fe3ef&v=4" alt="Francis Carvalho" />
+
+                <strong>Francis Carvalho</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/55095394?s=460&u=81de13f8b108c2e90244398e64a77fee679fe3ef&v=4" alt="Francis Carvalho" />
+
+                <strong>Francis Cavalho</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+        </Schedule>
+
+        <Calendar>
+          <DayPicker
+          weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+          fromMonth={new Date()}
+          disabledDays={[{daysOfWeek: [0, 6]}]}
+          modifiers={{
+            available: {daysOfWeek: [1, 2, 3, 4, 5]},
+          }}
+          selectedDays={selectedDate}
+          onDayClick={handleDayChange}
+          months={[
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro',
+          ]}
+        />
+        </Calendar>
+      </Content>
     </Container>
   );
 };
